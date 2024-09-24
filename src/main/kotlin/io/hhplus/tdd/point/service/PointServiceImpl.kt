b6fa09ecdd73b1999735b426304b312a96e6b6bc
cheoljin <cheoljin1234@naver.com>
2024-09-24 23:47:01 +0900

    [Refactor: 포인트 유효성 관련 리팩토링] - 유효성 검사 내재화로 인한 서비스 코드 불필요 로직 제거

--- src/main/kotlin/io/hhplus/tdd/point/service/PointServiceImpl.kt
+++ src/main/kotlin/io/hhplus/tdd/point/service/PointServiceImpl.kt
@@ -1,17 +1,13 @@
 package io.hhplus.tdd.point.service
 
-import io.hhplus.tdd.comon.SynchronizedContainer
 import io.hhplus.tdd.database.PointHistoryTable
 import io.hhplus.tdd.database.UserPointTable
 import io.hhplus.tdd.point.domain.PointHistory
 import io.hhplus.tdd.point.domain.TransactionType
 import io.hhplus.tdd.point.domain.UserPoint
 import io.hhplus.tdd.point.domain.req.UserPointRequest
-import io.hhplus.tdd.point.exception.NotEnoughPointsException
-import io.hhplus.tdd.point.exception.PointOverflowException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 
 @Service
@@ -27,20 +23,12 @@ class PointServiceImpl(
     }
 
     override fun charge(userPointRequest: UserPointRequest): UserPoint {
-        userPointRequest.validate()
-
         val userPoint = userPointTable.selectById(userPointRequest.userId)
 
-        // Long 타입의 범위를 넘는지 체크
-        val newPoint = try {
-            Math.addExact(userPoint.point, userPointRequest.amount)
-        } catch (e: ArithmeticException) {
-            logger.error("original: ${userPoint.point} + new: ${userPointRequest.amount} = ${userPoint.point + userPointRequest.amount}")
-            throw PointOverflowException()
-        }
-
         return pointLockContainer.withLock(userPoint.id) {
+            val newPoint = userPoint.charge(userPointRequest.amount)
             val result = userPointTable.insertOrUpdate(userPointRequest.userId, newPoint)
+
             userPointHistoryTable.insert(userPointRequest.userId, userPointRequest.amount, TransactionType.CHARGE, System.currentTimeMillis())
             logger.info("포인트 충전 완료: userId: ${userPointRequest.userId}, amount: ${userPointRequest.amount}, 총 포인트: ${result.point}")
             result
@@ -48,19 +36,12 @@ class PointServiceImpl(
     }
 
     override fun use(userPointRequest: UserPointRequest): UserPoint {
-        userPointRequest.validate()
-
         val userPoint = userPointTable.selectById(userPointRequest.userId)
 
-        val newPoint = userPoint.point - userPointRequest.amount
-
-        if (newPoint < 0) {
-            logger.warn("현재 포인트: ${userPoint.point}, 사용하려는 포인트: ${userPointRequest.amount} = ${userPoint.point - userPointRequest.amount} <")
-            throw NotEnoughPointsException()
-        }
-
         return pointLockContainer.withLock(userPoint.id) {
+            val newPoint = userPoint.use(userPointRequest.amount)
             val result = userPointTable.insertOrUpdate(userPointRequest.userId, newPoint)
+
             userPointHistoryTable.insert(userPointRequest.userId, userPointRequest.amount, TransactionType.USE, System.currentTimeMillis())
             logger.info("포인트 사용 완료: userId: ${userPointRequest.userId}, amount: ${userPointRequest.amount}, 총 포인트: ${result.point}")
 
--- src/test/kotlin/io/hhplus/tdd/service/PointServiceTest.kt
+++ src/test/kotlin/io/hhplus/tdd/service/PointServiceTest.kt
@@ -6,16 +6,20 @@ import io.hhplus.tdd.point.domain.TransactionType
 import io.hhplus.tdd.point.domain.req.UserPointRequest
 import io.hhplus.tdd.point.exception.NotEnoughPointsException
 import io.hhplus.tdd.point.exception.PointOverflowException
+import io.hhplus.tdd.point.service.PointLockContainer
 import io.hhplus.tdd.point.service.PointServiceImpl
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import java.util.concurrent.CompletableFuture
 
 class PointServiceTest {
 
     private lateinit var userPointTable: UserPointTable
     private lateinit var userPointHistoryTable: PointHistoryTable
+    private lateinit var userPointLockContainer: PointLockContainer
     private lateinit var pointService: PointServiceImpl
 
     @BeforeEach
@@ -28,9 +32,12 @@ class PointServiceTest {
         userPointHistoryTable = PointHistoryTable()
         userPointHistoryTable.insert(userPoint.id, userPoint.point, TransactionType.CHARGE, now)
 
-        pointService = PointServiceImpl(userPointTable, userPointHistoryTable)
+        userPointLockContainer = PointLockContainer()
+
+        pointService = PointServiceImpl(userPointTable, userPointHistoryTable, userPointLockContainer)
     }
 
+    @DisplayName("UserPoint가 등록되지 않은 사용자의 포인트는 0이다")
     @Test
     fun `UserPoint가 등록되지 않은 사용자의 포인트는 0이다`() {
         val notExistUserPoint = pointService.getUserPoint(999)
@@ -38,6 +45,7 @@ class PointServiceTest {
         assertEquals(notExistUserPoint.point, 0)
     }
 
+    @DisplayName("UserPoint가 등록되지 않은 사용자의 히스토리는 존재하지 않는다")
     @Test
     fun `UserPoint가 등록되지 않은 사용자의 히스토리는 존재하지 않는다`() {
         val notExistUserPointHistories = pointService.getPointHistories(999)
@@ -45,6 +53,7 @@ class PointServiceTest {
         assertEquals(notExistUserPointHistories.size, 0)
     }
 
+    @DisplayName("현재 등록된 User의 UserPoint는 1이다")
     @Test
     fun `현재 등록된 User의 UserPoint는 1이다`() {
         val notExistUserPoint = pointService.getUserPoint(0)
@@ -52,6 +61,7 @@ class PointServiceTest {
         assertEquals(notExistUserPoint.point, 1)
     }
 
+    @DisplayName("UserPoint가 등록된 사용자의 히스토리는 존재한다")
     @Test
     fun `UserPoint가 등록된 사용자의 히스토리는 존재한다`() {
         val notExistUserPointHistories = pointService.getPointHistories(0)
@@ -73,6 +83,7 @@ class PointServiceTest {
         assertEquals("포인트의 범위가 지원하는 범위를 넘어섰습니다.", exception.message)
     }
 
+    @DisplayName("보유한 포인트 초과 사용으로 인한 포인트 사용 실패")
     @Test
     fun `보유한 포인트 초과 사용으로 인한 포인트 사용 실패`() {
         // given
@@ -85,5 +96,40 @@ class PointServiceTest {
 
         // then
         assertEquals("포인트가 부족합니다.", exception.message)
+    }
+
+    @DisplayName("동시 포인트사용: 1, -2, 1, 3, -2, -1")
+    @Test
+    fun `동시 포인트 사용 결과 포인트는 기존과 동일해야 한다`() {
+        // given
+        val userPoint = userPointTable.selectById(0)
+        pointService.charge(UserPointRequest.of(userPoint.id, 4)) // 추가로 4를 더해줌
+
+        // when
+        val futures = listOf(
+            CompletableFuture.runAsync {
+                Thread.sleep(10)
+                pointService.charge(UserPointRequest.of(userPoint.id, 1))
+            },
+            CompletableFuture.runAsync {
+                Thread.sleep(15)
+                pointService.use(UserPointRequest.of(userPoint.id, 6))
+            },
+            CompletableFuture.runAsync {
+                Thread.sleep(20)
+                pointService.charge(UserPointRequest.of(userPoint.id, 6))
+            },
+            CompletableFuture.runAsync {
+                Thread.sleep(23)
+                pointService.use(UserPointRequest.of(userPoint.id, 2))
+            },
+        )
+
+        CompletableFuture.allOf(*futures.toTypedArray()).join()
+
+        val newUserPoint = pointService.getUserPoint(userPoint.id)
+
+        // then
+        assertEquals(newUserPoint.point, 4)
     }
 }
