a3227c802cd80ff17f995135fa4d388b6d7e5e69
cheoljin <cheoljin1234@naver.com>
2024-09-25 22:10:07 +0900

    [Refactor: 불필요 로직 제거] - 로깅 관련 불필요 로직 제거

--- src/main/kotlin/io/hhplus/tdd/point/domain/UserPoint.kt
+++ src/main/kotlin/io/hhplus/tdd/point/domain/UserPoint.kt
@@ -2,21 +2,16 @@ package io.hhplus.tdd.point.domain
 
 import io.hhplus.tdd.point.exception.NotEnoughPointsException
 import io.hhplus.tdd.point.exception.PointOverflowException
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 
 data class UserPoint(
     val id: Long,
     val point: Long,
     val updateMillis: Long,
 ) {
-    private val logger: Logger = LoggerFactory.getLogger(javaClass)
-
     fun use(amount: Long): Long {
         val newPoint = this.point - amount
 
         if (newPoint < 0) {
-            logger.warn("현재 포인트: ${this.point}, 사용하려는 포인트: $amount = ${this.point - amount} <")
             throw NotEnoughPointsException()
         }
 
@@ -28,7 +23,6 @@ data class UserPoint(
         val newPoint = try {
             Math.addExact(this.point, amount)
         } catch (e: ArithmeticException) {
-            logger.error("original: ${this.point} + new: $amount = ${this.point + amount}")
             throw PointOverflowException()
         }
 
